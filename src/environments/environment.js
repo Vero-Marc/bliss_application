ce0488a124fb8b400db3b56b0fbb92d6fbed81ca
Vero-Marc <veronicamarcline60@gmail.com>
2024-11-21 21:20:45 +0530

    changes

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
-    // API_ENDPOINT : "http://127.0.0.1:8000/biz"
-    API_ENDPOINT : "https://thepurebliss.in/api/biz"
+    API_ENDPOINT : "http://127.0.0.1:8000/biz"
+    // API_ENDPOINT : "https://thepurebliss.in/api/biz"
 }
 
